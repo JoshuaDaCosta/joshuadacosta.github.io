c5d44ec424b6217a8a3b3eb2576e30658d05ba53
joshuadacosta <joshuacosta120@gmail.com>
2025-08-23 07:09:52 +0100

    salvando alguns arquivos

---
+++ static/js/index.js
@@ -0,0 +1,7 @@
+var navbar = document.querySelector("nav>ul")
+var button=document.querySelector(".botoes>a:nth-child(1)")
+
+button.addEventListener("click", ()=>{
+    navbar.style.display="none"
+})
+
