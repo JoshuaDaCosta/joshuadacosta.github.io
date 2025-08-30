a3d1321a4285141eec57fdf88ecfae42bf279cae
joshuadacosta <joshuacosta120@gmail.com>
2025-08-30 21:04:10 +0100

    primeira versão do portefólio sem mediaquerys

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,14 @@
+indow.onload = () => {
+    window.scrollTo(0, 0);
+  };
+
+
+
+
 var navbar = document.querySelector("nav>ul")
 var button=document.querySelector(".botoes>a:nth-child(1)")
 
-button.addEventListener("click", ()=>{
-    navbar.style.display="none"
-})
+// button.addEventListener("click", ()=>{
+//     navbar.style.display="none"
+// })
 
