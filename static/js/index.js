1c6e7af83fceb890c7383c3b64aa0cca4ff94ecc
joshuadacosta <joshuacosta120@gmail.com>
2025-10-02 20:33:50 +0100

    adicionando outros arquivos e novas actualizações

--- static/js/index.js
+++ static/js/index.js
@@ -1,14 +1,89 @@
-indow.onload = () => {
+window.onload = () => {
     window.scrollTo(0, 0);
   };
 
 
 
 
-var navbar = document.querySelector("nav>ul")
-var button=document.querySelector(".botoes>a:nth-child(1)")
+var navbar = document.querySelector("nav>ol>ul")
+var button=document.getElementById("dropdown")
+var nav=document.querySelector("header>nav")
+var buttonclose=document.getElementById("dropdownx")
+var navol=document.querySelector("nav>ol")
 
-// button.addEventListener("click", ()=>{
-//     navbar.style.display="none"
-// })
+var inicio=document.querySelector("nav>ol>ul>li:nth-child(1)")
+var sobre=document.querySelector("nav>ol>ul>li:nth-child(2)")
+var habilidades=document.querySelector("nav>ol>ul>li:nth-child(3)")
+var experiencias=document.querySelector("nav>ol>ul>li:nth-child(4)")
+var projectos=document.querySelector("nav>ol>ul>li:nth-child(5)")
+var contactos=document.querySelector("nav>ol>ul>li:nth-child(6)")
+
+button.addEventListener("click", ()=>{
+    navbar.style.display="flex";
+    button.style.display="none"
+    nav.style.flexDirection="column";
+    navol.style.height="auto"
+    navol.style.width="100%"
+
+})
+
+
+buttonclose.addEventListener("click", ()=>{
+    navbar.style.display="none";
+    button.style.display="block";
+    nav.style.flexDirection="row"
+    navol.style.height="auto"
+    navol.style.width="auto"
+
+})
+
+inicio.addEventListener("click", ()=>{
+    navbar.style.display="none";
+    button.style.display="block";
+    nav.style.flexDirection="row"
+    navol.style.height="auto"
+    navol.style.width="auto"
+
+})
+sobre.addEventListener("click", ()=>{
+    navbar.style.display="none";
+    button.style.display="block";
+    nav.style.flexDirection="row"
+    navol.style.height="auto"
+    navol.style.width="auto"
+
+})
+habilidades.addEventListener("click", ()=>{
+    navbar.style.display="none";
+    button.style.display="block";
+    nav.style.flexDirection="row"
+    navol.style.height="auto"
+    navol.style.width="auto"
+
+})
+experiencias.addEventListener("click", ()=>{
+    navbar.style.display="none";
+    button.style.display="block";
+    nav.style.flexDirection="row"
+    navol.style.height="auto"
+    navol.style.width="auto"
+
+})
+projectos.addEventListener("click", ()=>{
+    navbar.style.display="none";
+    button.style.display="block";
+    nav.style.flexDirection="row"
+    navol.style.height="auto"
+    navol.style.width="auto"
+
+})
+contactos.addEventListener("click", ()=>{
+    navbar.style.display="none";
+    button.style.display="block";
+    nav.style.flexDirection="row"
+    navol.style.height="auto"
+    navol.style.width="auto"
+
+})
+
 
